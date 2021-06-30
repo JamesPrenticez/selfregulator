205b7daf4039ef39e3f236f87e0e2617eee13a92
James-Prentice <Jamesprenticez@gmail.com>
2021-07-01 00:00:13 +1200

    Redirect

--- configureAmplify.js
+++ configureAmplify.js
@@ -1,7 +1,13 @@
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
+
 Amplify.configure({
     ...config,
     ssr: true
     }
 )
+
+if (typeof window !== 'undefined') {
+  config.oauth.redirectSignIn = `${window.location.origin}/user/profile/`;
+  config.oauth.redirectSignOut = `${window.location.origin}/user/profile/`;
+}
